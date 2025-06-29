7572375c2eb666d5fd135b46ea4c36f7960b2bde
ilya-kirbizhekov <ilyoniss@mail.ru>
2025-06-29 11:15:06 +1000

    rebase

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://taskuser:taskpassword@database:5432/taskdb'
