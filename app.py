1eb7efd3cd4e8b4af0abd4615c332ce9824f56d3
ilya-kirbizhekov <ilyoniss@mail.ru>
2025-06-29 11:31:13 +1000

    rebase

---
+++ app.py
@@ -0,0 +1,20 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+       self.send_response(200)
+       self.send_header("Content-type", "text/html")
+       self.end_headers()
+       self.wfile.write(b"<html><body><h1> Hello MF </h1></body></html>")
+
+def run(server_class = HTTPServer, handler_class = SimpleHandler, port = 8080):
+       server_address = ('',port)
+       httpd = server_class(server_address, handler_class)
+       print ("starting MF server")
+       httpd.serve_forever()
+
+if __name__ == '__main__':
+       run()
+
+
